c27cff0bc1ade6dda46150923d8439cc77a09e71
chenhailong <chenhailong@anji-plus.com>
2020-12-15 17:53:46 +0800

    格式

--- lib/aj_flutter_appsp.dart
+++ lib/aj_flutter_appsp.dart
@@ -4,18 +4,18 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
-// Copyright © 2018 anji-plus
-// 安吉加加信息技术有限公司
-// http://www.anji-plus.com
-// All rights reserved.
-// AppSp功能
+/// Copyright © 2018 anji-plus
+/// 安吉加加信息技术有限公司
+/// http:///www.anji-plus.com
+/// All rights reserved.
+/// AppSp功能
 class AjFlutterAppSp {
   static const MethodChannel _channel = const MethodChannel('aj_flutter_appsp');
 
-  //设置基础地址，如果在开发测试场景会用到，生产时候记得改成生产地址，或者最好不要对暴露set方法
-  //[appKey] 应用唯一标识
-  //[host] 设置请求基础地址
-  //[debug] 是否打开日志开关，true为打开
+  ///设置基础地址，如果在开发测试场景会用到，生产时候记得改成生产地址，或者最好不要对暴露set方法
+  ///[appKey] 应用唯一标识
+  ///[host] 设置请求基础地址
+  ///[debug] 是否打开日志开关，true为打开
   static Future<String> init(
       {String appKey, String host, bool debug = true}) async {
     final String result = await _channel.invokeMethod(
@@ -23,7 +23,7 @@ class AjFlutterAppSp {
     return result;
   }
 
-  //获取版本信息
+  ///获取版本信息
   static Future<SpRespUpdateModel> getUpdateModel() async {
     final String jsonStr = await _channel.invokeMethod('getUpdateModel');
     SpRespUpdateModel updateModel =
@@ -31,7 +31,7 @@ class AjFlutterAppSp {
     return updateModel;
   }
 
-  //获取公告信息
+  ///获取公告信息
   static Future<SpRespNoticeModel> getNoticeModel() async {
     final String jsonStr = await _channel.invokeMethod('getNoticeModel');
     SpRespNoticeModel noticeModel =
